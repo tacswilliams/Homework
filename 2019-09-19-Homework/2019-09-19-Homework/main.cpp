8f881102c31a48969746ba5d09dec7e762b7d819
tacswilliams <55443380+tacswilliams@users.noreply.github.com>
2019-09-19 08:38:52 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/main.cpp
@@ -0,0 +1,14 @@
+// Name: Mr. Williams
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include <iostream>
+
+int main()
+{
+	std::cout	<< "Name: Mr. Williams" << std::endl 
+				<<"Period 1: Class Name" << std::endl
+				<< "Period 2: ";
+}
